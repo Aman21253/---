ad471f4cf3e9383dc29f20784fb5edfadc57dca7
Aman <amanabbansal@gmail.com>
2024-09-11 10:59:57 +0530

    UI DONE

--- i.js
+++ i.js
@@ -1,13 +1,11 @@
 
-let slideIndex = 0;
-showSlides()
-function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000);
-}
+// Simulating an incoming message
+document.querySelector('.notification-bell').addEventListener('click', () => {
+    let notificationCount = document.querySelector('.notification-count');
+    let currentCount = parseInt(notificationCount.textContent);
+
+    // Increment count and display
+    currentCount += 1;
+    notificationCount.textContent = currentCount;
+    document.querySelector('.notification-bell').classList.add('has-new');
+});
